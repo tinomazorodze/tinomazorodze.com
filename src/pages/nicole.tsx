374b9ec077b5116f49187f5fd6f64e7f7d047c36
Tino Mazorodze <tinomazorodze.mt@gmail.com>
2023-10-31 11:36:45 +0200

    nicole update

--- src/pages/nicole.tsx
+++ src/pages/nicole.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image"
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
+import JSConfetti from 'js-confetti'
 
 export default function Nicole() {
   const audioRef: any = useRef();
+
+  useEffect(() => {
+    const jsConfetti = new JSConfetti()
+    jsConfetti.addConfetti()
+    audioRef.current.play();
+  })
 
   return (
     <main className="h-screen w-screen bg-[#333] text-white">
@@ -11,7 +18,7 @@ export default function Nicole() {
       </div>
       <div className="p-4 text-4xl text-center">Happy Birthday <span className="bg-gradient-to-tr from-[var(--purple)] via-secondaryColor to-[var(--purple)] bg-clip-text text-transparent font-bold">Nae Nae</span></div>
       <audio ref={audioRef} id="player" autoPlay>
-        <source src="/media/hbd.mp3" />
+        <source src="media/hbd.mp3" />
       </audio>
       <div className="h-[50px] w-[200px] grid place-content-center bg-gradient-to-tr from-primaryColor via-secondaryColor to-primaryColor rounded-full mx-auto font-bold">Mabesty manyama</div>
     </main>
