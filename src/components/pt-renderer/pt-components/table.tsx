ca3a4fad4257bc1bde29bbb391ca5c222af761b9
Tino Mazorodze <tinomazorodze.mt@gmail.com>
2023-11-10 16:05:12 +0200

    table update

--- src/components/pt-renderer/pt-components/table.tsx
+++ src/components/pt-renderer/pt-components/table.tsx
@@ -3,19 +3,19 @@ export default function blockTable({ rows }: {
 }) {
 
   return (
-    <table>
+    <table className="w-full mb-6">
       <thead>
-        <tr>
-          {rows[0].split(',').map((cell) => (
-            <th key="cell">{cell}</th>
+        <tr className="bg-secondary text-primary-2">
+          {rows[0].split(',').map((cell, i) => (
+            <th key={i} className={`text-left px-2 py-2 ${i > 2 && " hidden md:table-cell"}`}>{cell}</th>
           ))}
         </tr>
       </thead>
       <tbody>
         {rows.slice(1).map((row, i) => (
-          <tr key={i}>
-            {row.split(',').map((cell) => (
-              <td key="cell">{cell}</td>
+          <tr key={i} className="odd:bg-primary even:bg-accent-2">
+            {row.split(',').map((cell, i) => (
+              <td key={i} className={`text-left px-2 py-2 ${i > 2 && " hidden md:table-cell"}`}>{cell}</td>
             ))}
           </tr>
         ))}
