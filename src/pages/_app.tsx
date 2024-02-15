e237ebcaf54aecf9a35c5a2f309f52ada5006dc3
Tino Mazorodze <tinomazorodze.mt@gmail.com>
2024-02-15 20:10:19 +0200

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,33 @@ import '@/styles/tailwind.css'
 import { AppProps } from 'next/app'
 import { Layout } from '@/components/Layout'
 import { Providers } from '@/components/providers'
+import { firePageView } from '@/lib/gtag'
+import { useEffect } from 'react'
+import { Analytics } from '@vercel/analytics/react'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    firePageView(window.location.pathname)
+  }, [])
+
   return (
-    <Providers>
-      <div className="flex w-full">
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </div>
-    </Providers>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <Providers>
+        <div className="flex w-full">
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </div>
+      </Providers>
+      <Analytics />
+    </>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,29 @@
 import { Head, Html, Main, NextScript } from 'next/document'
-import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function Document() {
   return (
     <Html lang="en" className="h-full antialiased" suppressHydrationWarning>
       <Head />
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id='${process.env.GA_SECRET}'`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.GA_SECRET}');
+        `,
+        }}
+      />
       <body className="flex h-full bg-zinc-50 dark:bg-black">
         <Main />
         <NextScript />
       </body>
-      <GoogleAnalytics gaId={`${process.env.GA_SECRET}`} />
     </Html>
   )
 }
