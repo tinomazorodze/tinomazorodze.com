198b15af124a3da3a21bfbd5f058c5e52a4365ce
Tino Mazorodze <tinomazorodze.mt@gmail.com>
2024-06-25 16:09:52 +0200

    table alt

--- src/components/pt-renderer/pt-components/table.tsx
+++ src/components/pt-renderer/pt-components/table.tsx
@@ -3,7 +3,7 @@ export default function blockTable({ rows }: { rows: string[] }) {
     <table className="mb-6 w-full">
       <thead>
         <tr className="bg-slate-900 text-white dark:bg-white dark:text-slate-900">
-          {rows[0].split(',').map((cell, i) => (
+          {rows[0].split('|').map((cell, i) => (
             <th
               key={i}
               className={`px-2 py-2 text-left ${i > 2 && ' hidden md:table-cell'}`}
@@ -19,7 +19,7 @@ export default function blockTable({ rows }: { rows: string[] }) {
             key={i}
             className="odd:bg-primary even:bg-zinc-100 dark:even:bg-white/10"
           >
-            {row.split(',').map((cell, i) => (
+            {row.split('|').map((cell, i) => (
               <td
                 key={i}
                 className={`px-2 py-2 text-left ${i > 2 && ' hidden md:table-cell'}`}
