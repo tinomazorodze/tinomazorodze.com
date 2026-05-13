af8bd27818d7c0c1b596d667aa081f1e04ab4ed9
Tino Mazorodze <tinomazorodze.mt@gmail.com>
2026-05-13 17:09:43 +0200

    removed photos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -275,8 +275,7 @@ export default async function Home() {
           </div>
         </div>
       </Container>
-      <Photos />
-      <Container className="mt-24 md:mt-28">
+      <Container className="mt-12 md:mt-14">
         <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
           <div className="flex flex-col gap-16">
             {articles.map((article) => (
